7950694937d97332355f7b1a35ad645d4ff5dda1
Jeet Parte <jeetparte@gmail.com>
2022-02-07 18:50:12 +0530

    Set root view controller programmatically

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -8,6 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    override func loadView() {
+        let v = UIView()
+        v.backgroundColor = .systemYellow
+        self.view = v
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
