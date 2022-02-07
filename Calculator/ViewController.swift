b6be151775ff1807f981b05a099ce61df889881d
Jeet Parte <jeetparte@gmail.com>
2022-02-07 19:37:34 +0530

    Create view scaffolds

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var resultsView: UIView!
+    var buttonsStackView: UIStackView!
     
     override func loadView() {
         let v = UIView()
@@ -16,9 +18,35 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
         self.view.backgroundColor = .systemBackground
-        self.overrideUserInterfaceStyle = .dark
+//        self.overrideUserInterfaceStyle = .dark
+        
+        let v = self.view!
+        self.buttonsStackView = UIStackView()
+        v.addSubview(buttonsStackView)
+        buttonsStackView.backgroundColor = .systemMint
+        
+        buttonsStackView.translatesAutoresizingMaskIntoConstraints = false
+        let marginsGuide = v.layoutMarginsGuide
+        NSLayoutConstraint.activate([
+            marginsGuide.leadingAnchor.constraint(equalTo: buttonsStackView.leadingAnchor),
+            marginsGuide.trailingAnchor.constraint(equalTo: buttonsStackView.trailingAnchor),
+            marginsGuide.bottomAnchor.constraint(equalTo: buttonsStackView.bottomAnchor),
+            
+            buttonsStackView.heightAnchor.constraint(equalTo: marginsGuide.heightAnchor, multiplier: 0.65) // might want to tweak this for landscape orientation
+        ])
+        
+        self.resultsView = UIView()
+        v.addSubview(resultsView)
+        resultsView.backgroundColor = .systemPurple
+        resultsView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            resultsView.leadingAnchor.constraint(equalTo: buttonsStackView.leadingAnchor),
+            resultsView.trailingAnchor.constraint(equalTo: buttonsStackView.trailingAnchor),
+            buttonsStackView.topAnchor.constraint(equalToSystemSpacingBelow: resultsView.bottomAnchor, multiplier: 1),
+            resultsView.topAnchor.constraint(equalTo: marginsGuide.topAnchor)
+        ])
     }
 
 
